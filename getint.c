d45154a364a7b9d2cf78989ccd7b9a1478c80456
rdmckinnon <ryan@ryanmckinnon.com>
2014-01-11 13:45:59 -0500

    upload

---
+++ getint.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Please give me an int: ");
+    int x = GetInt();
+    
+    printf("please give me another: ");
+    int y = GetInt();
+    
+    printf("The sum of %d and %d is %d.\n", x, y, x+ y);
+       
+}
