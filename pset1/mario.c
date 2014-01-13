51fe2b66914e331f6bdb29c1a8940c3ed35d1ac2
rdmckinnon <RyanM@Ryan-McKinnons-iMac.local>
2014-01-13 13:07:32 -0500

    pset1

---
+++ pset1/mario.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int n;
+
+do 
+{
+    n = GetInt();
+}
+while (n is invalid);
+
+for (int i = 0; i < height; i++)
+{
+    //print spaces
+    //print hashes
+    //print new line
+}
